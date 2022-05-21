4aa674c3cb055ebd34009934f868494ccdeb6d70
Laura <Laura@mail.com>
2022-05-20 22:30:36 -0300

    agrego estilos a newsletter

--- src/app/view-demo/view-demo.component.ts
+++ src/app/view-demo/view-demo.component.ts
@@ -16,48 +16,50 @@ export class ViewDemoComponent implements OnInit {
   url: String;
   formGroup: FormGroup;
   name: String;
-  username: String;
+  email: String;
 
-  constructor(protected router:Router, private formBuilder: FormBuilder,protected httpClient: HttpClient) { 
+  constructor(protected router: Router,
+    private formBuilder: FormBuilder,
+    protected httpClient: HttpClient) {
     console.log('viewdemo constructor');
 
   }
-  
+
   ngOnInit(): void {
     console.log('Iniciando appComponent');
     this.formGroup = this.formBuilder.group({
-      username: new FormControl('',  Validators.required),
-      name: new FormControl('',  Validators.required),
+      email: new FormControl('', Validators.required),
+      name: new FormControl('', Validators.required),
     });
   }
 
-  onClick(){
+  onClick() {
     console.log('ejecutando redirect en view demo');
     this.router.navigate(['/'])
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log('ejecutando submit');
-    console.log('username:' + this.formGroup.get('username').value);
-    this.username = this.formGroup.get('username').value;
+    console.log('email:' + this.formGroup.get('email').value);
+    this.email = this.formGroup.get('email').value;
   }
 
-  changeKitty(){
+  changeKitty() {
 
-    let res: Observable<Response[]> = 
+    let res: Observable<Response[]> =
       this.httpClient.get<Response[]>('https://api.thecatapi.com/v1/images/search')
-      .pipe(share());
-      //.pipe(catchError(this.handleError));
-    
-    res.subscribe(
-        value=> {
-            console.log('URL:' + value[0].url);
-            this.url = value[0].url;
-                }, 
-        error => { 
-          console.log('ocurrio un error');
+        .pipe(share());
+    //.pipe(catchError(this.handleError));
 
-        });
+    res.subscribe(
+      value => {
+        console.log('URL:' + value[0].url);
+        this.url = value[0].url;
+      },
+      error => {
+        console.log('ocurrio un error');
+
+      });
 
 
   }
