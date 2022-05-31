ac3aa433a1cf00032a92df971163099481906234
Laura <Laura@mail.com>
2022-05-31 19:53:18 -0300

    se agrega busqueda de productos. por el momento solo se puede filtrar por marca. Se busca por la palabra exacta

--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -8,14 +8,14 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor( protected router:Router) { }
+  constructor(protected router: Router) { }
 
   ngOnInit(): void {
   }
 
-  onClick(){
-    console.log('ejecutando redirect');
-    this.router.navigate(['/buscar-golosina'])
+  buscarGolosinas() {
+    let golosina = (<HTMLInputElement>document.getElementById("txtGolosinas")).value;
+    this.router.navigate(['/buscar-golosina', { golosina }]);
   }
 
 }
