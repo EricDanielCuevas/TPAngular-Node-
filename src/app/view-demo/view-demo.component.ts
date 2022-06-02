69c565427692ebbdcdbd9d884e4b1b38a5593e46
eric cuevas <erickummax@gmail.com>
2022-06-02 04:18:11 -0300

    boton volver al home newsletter funcionando

--- src/app/view-demo/view-demo.component.ts
+++ src/app/view-demo/view-demo.component.ts
@@ -35,7 +35,7 @@ export class ViewDemoComponent implements OnInit {
 
   onClick() {
     console.log('ejecutando redirect en view demo');
-    this.router.navigate(['/view'])
+    this.router.navigate(['/'])
   }
 
   onSubmit() {
