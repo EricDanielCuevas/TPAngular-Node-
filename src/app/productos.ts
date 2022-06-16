e6b97dbb83ba9a4edc9cfbcd6edbf226ef540a1f
eric cuevas <erickummax@gmail.com>
2022-06-16 20:05:06 -0300

    llega el articulo en promo nulo al carrito

--- src/app/productos.ts
+++ src/app/productos.ts
@@ -7,5 +7,7 @@ export const productos=[
 
       { id: 4, nombre: 'Chocolate', marca: 'Milka', descripcion: ' chocolate negro', precio: 233.2, cantidad: 1, disponible: true,imagen: './assets/arcor_menta_bolsa.png' },
 
-      { id: 5, nombre: 'Barritas', marca: 'Arcor', descripcion: 'avena con chocolate', precio: 233.2, cantidad: 1, disponible: true,imagen: './assets/arcor_miel_menta_bolsa.png' }
+      { id: 5, nombre: 'Barritas', marca: 'Arcor', descripcion: 'avena con chocolate', precio: 233.2, cantidad: 1, disponible: true,imagen: './assets/arcor_miel_menta_bolsa.png' },
+
+      { id: 6, nombre: 'combo-seños', marca: 'combo', descripcion: 'Cereal arcor, alfajor jorgito, jugo cepita, 25 caramelos surtidos, tableta beldent, cofler mini', precio: 233.2, cantidad: 1, disponible: true,imagen: './assets/combo-seños.png' }
 ];
